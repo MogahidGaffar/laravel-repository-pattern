248546406a836064281cca9f9a85f423b7bc1516
mogahidgaffar <mogahidgaffar@gmail.com>
2022-08-22 15:41:11 +0300

    views fininshing

--- resources/views/users/createUser.blade.php
+++ resources/views/users/createUser.blade.php
@@ -1,28 +1,38 @@
 @extends('users.layout')
 @section('content')
+<br><br><br>
 
 <form action="{{ route('users.createOne') }}" method="post">
   @csrf
+  <div class="row">
+    <div class="col-md-6">
     <div class="form-group">
       <label for="name"> @lang('words.Name')</label>
       <input type="text" class="form-control" id="name"  name="name">
     </div>
- 
+  </div>
+      <div class="col-md-6">
     <div class="form-group">
         <label for="address"> @lang('words.Address')</label>
         <input type="text" class="form-control" id="address" name="address">
       </div>
-
+    </div>
+  </div>
+      <div class="row">
+        <div class="col-md-6">
       <div class="form-group">
         <label for="phone"> @lang('words.Phone')</label>
         <input type="text" class="form-control" id="phone" name="phone" >
       </div>
+    </div>
 
-      
+        <div class="col-md-6">
       <div class="form-group">
         <label for="email"> @lang('words.Email')</label>
         <input type="text" class="form-control" id="email" name="email" >
       </div>
+      </div>
+    </div>
 
     <button type="submit" class="btn btn-primary">@lang('words.save')</button>
   </form>
